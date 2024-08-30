42d401942112a5e18dbe944cc6672685e80e76b2
Joel Filho <joelfilho02812@gmail.com>
2024-08-30 15:56:40 -0300

    chore(eureka-server): init project

---
+++ eureka-server/src/main/java/tech/joelf/eureka_server/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package tech.joelf.eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
