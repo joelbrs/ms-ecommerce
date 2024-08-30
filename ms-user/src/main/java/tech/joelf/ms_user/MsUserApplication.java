5c8b44921db6dcc75bace80718825d45db4f0c35
Joel Filho <joelfilho02812@gmail.com>
2024-08-30 16:05:37 -0300

    feat(ms-user): enable eureka client

--- ms-user/src/main/java/tech/joelf/ms_user/MsUserApplication.java
+++ ms-user/src/main/java/tech/joelf/ms_user/MsUserApplication.java
@@ -2,12 +2,13 @@ package tech.joelf.ms_user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class MsUserApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsUserApplication.class, args);
 	}
-
 }
