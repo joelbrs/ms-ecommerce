1c4873ab904d494458d39804a0418097ce5abe4e
Joel Filho <joelfilho02812@gmail.com>
2024-08-30 15:59:11 -0300

    feat(eureka-server): enable eureka server

--- eureka-server/src/main/java/tech/joelf/eureka_server/EurekaServerApplication.java
+++ eureka-server/src/main/java/tech/joelf/eureka_server/EurekaServerApplication.java
@@ -2,12 +2,13 @@ package tech.joelf.eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
-
 }
