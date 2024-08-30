48ca413077be62d08c0dafb85a4c927d4ac2db6b
Joel Filho <joelfilho02812@gmail.com>
2024-08-30 16:04:27 -0300

    chore(ms-user): init project

---
+++ ms-user/src/main/java/tech/joelf/ms_user/MsUserApplication.java
@@ -0,0 +1,13 @@
+package tech.joelf.ms_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsUserApplication.class, args);
+	}
+
+}
---
+++ ms-user/src/test/java/tech/joelf/ms_user/MsUserApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.joelf.ms_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
