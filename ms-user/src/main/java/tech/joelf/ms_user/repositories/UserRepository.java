844db223f25e365090c5f660210bbc4389f6544a
Joel Filho <joelfilho02812@gmail.com>
2024-08-30 16:59:34 -0300

    feat(ms-user): add pageable as param in findUsers method

--- ms-user/src/main/java/tech/joelf/ms_user/repositories/UserRepository.java
+++ ms-user/src/main/java/tech/joelf/ms_user/repositories/UserRepository.java
@@ -1,6 +1,7 @@
 package tech.joelf.ms_user.repositories;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -11,5 +12,5 @@ import tech.joelf.ms_user.model.User;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query("SELECT u FROM User u WHERE LOWER(u.name) LIKE CONCAT('%', LOWER(:name), '%')")
-    Page<User> findUsers(String name);
+    Page<User> findUsers(Pageable pageable, String name);
 }
