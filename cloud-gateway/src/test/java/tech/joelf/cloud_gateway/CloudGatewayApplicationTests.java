0b8e66b0f72cb892f1ad2399efe915788897e859
Joel Filho <joelfilho02812@gmail.com>
2024-09-01 10:22:16 -0300

    chore(cloud-gateway): init project

---
+++ cloud-gateway/src/test/java/tech/joelf/cloud_gateway/CloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.joelf.cloud_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
