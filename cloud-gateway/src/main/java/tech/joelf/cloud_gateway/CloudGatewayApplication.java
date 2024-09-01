624a76df797bf2095937b7ada5dcf3f95bb27f72
Joel Filho <joelfilho02812@gmail.com>
2024-09-01 10:33:19 -0300

    chore(gateway): add gateway configs

--- cloud-gateway/src/main/java/tech/joelf/cloud_gateway/CloudGatewayApplication.java
+++ cloud-gateway/src/main/java/tech/joelf/cloud_gateway/CloudGatewayApplication.java
@@ -2,7 +2,14 @@ package tech.joelf.cloud_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
+@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CloudGatewayApplication {
 
@@ -10,4 +17,15 @@ public class CloudGatewayApplication {
 		SpringApplication.run(CloudGatewayApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(r -> r.path("/users/**")
+						.uri("lb://ms-user"))
+				.route(r -> r.path("/products/**")
+						.uri("lb://ms-product"))
+				.route(r -> r.path("/categories")
+						.uri("lb://ms-category"))
+				.build();
+	}
 }
