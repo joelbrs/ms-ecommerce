88b703486e6e30ecdd2c9cfde0888751d77b1bfb
Joel Filho <joelfilho02812@gmail.com>
2024-09-01 08:59:26 -0300

    infra(ms-product): add product repository

---
+++ ms-product/src/main/java/tech/joelf/ms_product/repositories/ProductRepository.java
@@ -0,0 +1,16 @@
+package tech.joelf.ms_product.repositories;
+
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import tech.joelf.ms_product.model.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query("SELECT p FROM Product p WHERE LOWER(p.name) LIKE CONCAT('%', LOWER(:name), '%')")
+    Page<Product> findProducts(Pageable pageable, String name);
+}
