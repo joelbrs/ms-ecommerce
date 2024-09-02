38525248f98aeba2ca389a41d4b761a376bf454d
Joel Filho <joelfilho02812@gmail.com>
2024-09-02 09:28:21 -0300

    chore(ms-relations): init project

---
+++ ms-relations/src/main/java/tech/joelf/ms_relations/MsRelationsApplication.java
@@ -0,0 +1,12 @@
+package tech.joelf.ms_relations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsRelationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsRelationsApplication.class, args);
+	}
+}
