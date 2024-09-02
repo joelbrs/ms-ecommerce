79b51c224fb98afc0ff9ddd0a40b4f2c25571352
Joel Filho <joelfilho02812@gmail.com>
2024-09-02 09:32:37 -0300

    chore(ms-relations): add eureka client configs & dev profile

--- ms-relations/src/main/java/tech/joelf/ms_relations/MsRelationsApplication.java
+++ ms-relations/src/main/java/tech/joelf/ms_relations/MsRelationsApplication.java
@@ -2,7 +2,9 @@ package tech.joelf.ms_relations;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class MsRelationsApplication {
 
